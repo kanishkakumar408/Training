f8077d4307aa384af267f6c6263dac851365bf02
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-11-05 13:12:45 +0530

    ifmodifier and unless modifier

---
+++ ruby/basics/ifmodifier.rb
@@ -0,0 +1,10 @@
+number=1000
+verified=true
+
+  if number>500 && verified
+     puts "huge number"
+  end
+
+#Or with if modifier
+
+puts "huge number" if number>500 && verified 
---
+++ ruby/basics/unlessmodifer.rb
@@ -0,0 +1,9 @@
+x=8
+unless x>10
+  puts "no is less than 10"
+end
+
+# or with unless modifier
+
+x=9
+puts "no is less than 10" unless x>10
