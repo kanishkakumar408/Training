83e7c491ff7387ac919d5abe5397ba61483e2867
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-11-06 11:24:17 +0530

    program of comparable method & to convert time object into other object

---
+++ ruby/DateTime/Timeobject_to_otherobject.rb
@@ -0,0 +1,5 @@
+someday = Time.new(2000,4,15)
+
+puts someday.to_i
+puts someday.ctime
+puts someday.to_a
---
+++ ruby/DateTime/comparablemethod.rb
@@ -0,0 +1,8 @@
+birthday=Time.new(2020,5,19)
+summer=Time.new(2020,4,15)
+independence_day=Time.new(2020,8,15)
+winter=Time.new(2020,11,25)
+
+puts birthday<=independence_day
+puts winter>summer
+puts birthday !=winter
