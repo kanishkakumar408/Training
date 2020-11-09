50821c57530919fd8d302b51c428a8bb86ff7d5b
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-11-09 17:51:42 +0530

    program of number in pattern from of right triangle

---
+++ ruby/Programs/Nointriangle.rb
@@ -0,0 +1,8 @@
+puts "enter the number:"
+n=gets.chomp.to_i
+(n+1).times do |i|
+     i.times do |j|
+        print i
+     end
+     print "\n"
+end
---
+++ ruby/Programs/Nointriangle2.rb
@@ -0,0 +1,8 @@
+puts "enter the number of rows:"
+n=gets.chomp.to_i
+(n+1).times do |i|
+      i.times do |j|
+          print j+1
+      end
+      print "\n"
+end 
