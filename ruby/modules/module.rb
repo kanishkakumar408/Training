81de0288a7e2c6b92189b1adee417b4382c61cce
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-11-10 13:15:14 +0530

    programs to create modules

---
+++ ruby/modules/module.rb
@@ -0,0 +1,24 @@
+
+module IPL 
+      
+    C = 20; 
+    def IPL.portal 
+        puts "Welcome to IPl Portal!"
+    end
+        
+  
+    def IPL.match   
+        puts "first match between rcb and mi!"
+    end
+        
+    def IPL.final   
+        puts "final between delhi and mumbai"
+    end
+end
+   
+puts IPL::C
+  
+IPL.portal 
+IPL.match 
+Gfg.final 
+
