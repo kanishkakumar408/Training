2bc65d89efc398121b308ba497bc3b9428f15bc3
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-11-11 17:16:39 +0530

    printing alphabet B letter

---
+++ ruby/Programs/B.rb
@@ -0,0 +1,13 @@
+puts "enter the number of rows:"
+r=gets.chomp.to_i
+s=3
+r.times do |i|
+   s.times do |j|
+       if (j==1 and i==1)or(j==1 and i==3)
+           print ' '
+        else 
+            print "@"
+        end   
+    end
+    print "\n"
+end    
