d13cbbb35a1a12d824da53f6678424748937a724
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-04 16:39:02 +0530

    Created a new rails app Omega, configured database, created models category & product

---
+++ rails/Omega/db/migrate/20210104105414_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.references :category, null: false, foreign_key: true
+      t.text :description
+      t.decimal :price
+      t.integer :make
+
+      t.timestamps
+    end
+  end
+end
